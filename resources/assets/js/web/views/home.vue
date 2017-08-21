1b8258ae2d7860151bfb02bb3f967d54ac9880af
Lenbo <664967337@qq.com>
2017-08-21 17:58:42 +0800

    更新路由跳转方法

--- resources/assets/js/web/views/home.vue
+++ resources/assets/js/web/views/home.vue
@@ -244,16 +244,17 @@
                 self.parseLoad = true;
                 self.$http.post('/loan/parse', this.form, {unload: true}).then(resp => {
                     if (resp.body.code === 0) {
-                        setTimeout(() => {
-                            self.parseLoad = false;
-                            zhuge.track('快速分析', {
-                                '姓名': self.form.name,
-                                '身份证': self.form.id_card,
-                                '手机号': window.app.userInfo.username,
-                                '借贷金额': self.form.loan_amount,
-                            });
+                        self.parseLoad = false;
+                        zhuge.track('快速分析', {
+                            '姓名': self.form.name,
+                            '身份证': self.form.id_card,
+                            '手机号': window.app.userInfo.username,
+                            '借贷金额': self.form.loan_amount,
+                        });
 
-                            self.$router.push({path: '/loan/cases'});
+                        setTimeout(() => {
+                            window.location.hash = '/loan/cases';
+//                            self.$router.push({path: '/loan/cases'});
                         }, 1000);
                     } else {
                         self.parseLoad = false;
@@ -336,16 +337,16 @@
                 this.globalData = config;
             }
 
-//            this.form = {
-//                name: '林博',
-//                id_card: '123123123123123123',
-//                loan_amount: 100,
-//                loan_deadline: 1,
-//                loan_deadline_type: '日',
-//                use_loan_time: '2017-08-08',
-//                job: 1,
-//                more_info: [1]
-//            };
+            this.form = {
+                name: '林博',
+                id_card: '123123123123123123',
+                loan_amount: 100,
+                loan_deadline: 1,
+                loan_deadline_type: '日',
+                use_loan_time: '2017-08-08',
+                job: 1,
+                more_info: [1]
+            };
 
             this.updateNotify();
         }
